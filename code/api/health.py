9ee7911a40c77f9a00ec433489e59a1f0c1419cc
Taras Maliuchenko <tmali@softserveinc.com>
2021-04-15 20:54:41 +0300

    Add aws guard duty driver Add environment variables Add aws guard duty health check logic

--- code/api/health.py
+++ code/api/health.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from api.observables import Observable
+from flask import Blueprint, current_app
 from api.utils import get_jwt, jsonify_data
+from aws_drivers.aws_guard_duty_driver import GuardDutyDriver
+
 
 health_api = Blueprint('health', __name__)
 
@@ -7,4 +10,12 @@ health_api = Blueprint('health', __name__)
 @health_api.route('/health', methods=['POST'])
 def health():
     _ = get_jwt()
+    GDD = GuardDutyDriver()
+    type_, value = current_app.config['HEALTH_CHECK_OBSERVABLE'].values()
+
+    observable = Observable.of(type_)
+    condition = observable.query(value)
+
+    GDD.findings.list(current_app.config['AWS_GUARD_DUTY_DETECTOR_ID'], condition)
+
     return jsonify_data({'status': 'ok'})
