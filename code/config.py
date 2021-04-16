a8940f9cd09613167087878321a960fab0f6d387
Taras Maliuchenko <tmali@softserveinc.com>
2021-04-16 15:16:24 +0300

    Delete default aws region variable

--- code/config.py
+++ code/config.py
@@ -5,5 +5,4 @@ class Config:
     settings = json.load(open('container_settings.json', 'r'))
     VERSION = settings['VERSION']
 
-    DEFAULT_AWS_REGION = 'us-east-1'
     DEFAULT_CTR_ENTITIES_LIMIT = 100
