7282aa395c542ad2d9652dc68d2580e474a76ba3
Taras Maliuchenko <tmali@softserveinc.com>
2021-04-27 15:00:11 +0300

    Fix autotests Remove unnecessary logic from health endpoint

--- code/api/health.py
+++ code/api/health.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
-from api.observables import Observable
 from api.utils import get_jwt, jsonify_data
 from aws_drivers.aws_guard_duty_driver import GuardDutyDriver
 
@@ -10,10 +9,6 @@ health_api = Blueprint('health', __name__)
 @health_api.route('/health', methods=['POST'])
 def health():
     _ = get_jwt()
-    guard_duty = GuardDutyDriver()
-    observable = Observable.of(type_='ip')
-    criterion = observable.query(observable='1.1.1.1')
-    guard_duty.findings.list_by(criterion)
-    _ = guard_duty.findings.get()
+    _ = GuardDutyDriver()
 
     return jsonify_data({'status': 'ok'})
