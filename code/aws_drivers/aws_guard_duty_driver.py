99d9bd3b5d3e9baa470e65f18472750927c3cad5
Taras Maliuchenko <tmali@softserveinc.com>
2021-05-19 12:52:02 +0300

    Handle ValueError error

--- code/aws_drivers/aws_guard_duty_driver.py
+++ code/aws_drivers/aws_guard_duty_driver.py
@@ -17,11 +17,14 @@ class GuardDutyDriver(object):
         self.region = str(current_app.config['AWS_REGION'])
         self.access_key = str(current_app.config['AWS_ACCESS_KEY_ID'])
         self.secret_key = str(current_app.config['AWS_SECRET_ACCESS_KEY'])
-        self.driver = boto3.client(
-            'guardduty', self.region,
-            aws_access_key_id=self.access_key,
-            aws_secret_access_key=self.secret_key
-        )
+        try:
+            self.driver = boto3.client(
+                'guardduty', self.region,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key
+            )
+        except ValueError as error:
+            raise GuardDutyError(error.args[0])
         self.findings = self.Finding(self)
 
     class Finding:
@@ -30,7 +33,10 @@ class GuardDutyDriver(object):
             self._findings = []
             self.max_results = 50
             self.driver = root.driver
-            self.ctr_limit = current_app.config['CTR_ENTITIES_LIMIT']
+            self.ctr_limit = \
+                int(current_app.config['CTR_ENTITIES_LIMIT']) \
+                if not ValueError \
+                else current_app.config['DEFAULT_CTR_ENTITIES_LIMIT']
             self.detector = current_app.config['AWS_GUARD_DUTY_DETECTOR_ID']
 
         def get(self):
