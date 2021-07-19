3f7f935f60a7bebf8903cf96379caaea5229c862
Taras Maliuchenko <tmali@softserveinc.com>
2021-07-19 13:54:01 +0300

    Handle wrong ctr entities limit and observable value

--- code/aws_drivers/aws_guard_duty_driver.py
+++ code/aws_drivers/aws_guard_duty_driver.py
@@ -35,7 +35,8 @@ class GuardDutyDriver(object):
             self.driver = root.driver
             try:
                 self.ctr_limit = int(current_app.config['CTR_ENTITIES_LIMIT'])
-            except ValueError:
+                assert self.ctr_limit > 0
+            except (ValueError, AssertionError):
                 self.ctr_limit = \
                     current_app.config['DEFAULT_CTR_ENTITIES_LIMIT']
             self.detector = current_app.config['AWS_GUARD_DUTY_DETECTOR_ID']
@@ -65,8 +66,11 @@ class GuardDutyDriver(object):
                             and finding not in self._findings:
                         self._findings.append(finding)
             except (BotoCoreError, ValueError, ClientError) as error:
+                if error.operation_name == 'ListFindings' and \
+                        error.response['Type'] == 'InternalException':
+                    return []
                 raise GuardDutyError(error.args[0])
 
             next_token = response.get('NextToken')
-            if next_token and len(self._findings) < self.ctr_limit:
+            if next_token and (0 < len(self._findings) < self.ctr_limit):
                 self.list_by(criterion, limit - self.max_results, next_token)
