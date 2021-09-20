b8622002d3519168add853d81395c08196e84825
Taras Maliuchenko <tmali@softserveinc.com>
2021-09-20 13:34:48 +0300

    Rename client class

--- code/api/health.py
+++ code/api/health.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from api.utils import get_jwt, jsonify_data
-from aws_drivers.aws_guard_duty_driver import GuardDutyDriver
+from api.client import GuardDuty
 
 
 health_api = Blueprint('health', __name__)
@@ -9,6 +9,6 @@ health_api = Blueprint('health', __name__)
 @health_api.route('/health', methods=['POST'])
 def health():
     _ = get_jwt()
-    client = GuardDutyDriver()
+    client = GuardDuty()
     _ = client.health()
     return jsonify_data({'status': 'ok'})
