46d6266a1c2dc30f539cbbbe38d4f0f1c77c8ca4
Taras Maliuchenko <tmali@softserveinc.com>
2021-09-21 21:41:45 +0300

    Restructure charts factory

--- code/api/charts/affected_instances.py
+++ code/api/charts/affected_instances.py
@@ -4,16 +4,96 @@ from api.charts.factory import IChart
 
 
 class AffectedInstances(IChart):
-    PERIODS = {
-        "last_24_hours": 1,
-        "last_7_days": 7,
-        "last_30_days": 30
-    }
+
+    def __init__(self):
+        self._type = "donut_graph"
+        self._id = "affected_instances"
+        self._title = "Affected instances"
+        self._periods = {
+            "last_24_hours": 1,
+            "last_7_days": 7,
+            "last_30_days": 30
+        }
+        self._default_period = "last_7_days"
+        self._description = (
+            "Affected Instances chart shows "
+            "what types of findings EC2 instances have."
+        )
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def periods(self):
+        return self._periods
+
+    @property
+    def default_period(self):
+        return self._default_period
+
+    @staticmethod
+    def affected_iscs_id(findings):
+        return list(
+            set(
+                [
+                    finding.Resource.InstanceDetails.InstanceId for finding
+                    in findings
+                ]
+            )
+        )
+
+    @staticmethod
+    def finding_types(findings):
+        return list(
+            set(
+                [finding.Type for finding in findings]
+            )
+        )
+
+    @staticmethod
+    def _sum_of_findings_by_isc(instance, findings):
+        return sum(
+            finding.Resource.InstanceDetails.InstanceId == instance for finding
+            in findings
+        )
+
+    def _segments(self, instance, findings):
+        def segment(_type):
+            return {
+                "key": self.finding_types(findings).index(_type),
+                "value": sum(
+                    finding.Type == _type and
+                    finding.Resource.InstanceDetails.InstanceId == instance
+                    for finding in findings
+                )
+            }
+
+        return [segment(_type) for _type in self.finding_types(findings)]
+
+    def _data(self, instance, findings):
+        return {
+            "key": self.affected_iscs_id(findings).index(instance),
+            "value": self._sum_of_findings_by_isc(instance, findings),
+            "segments": self._segments(instance, findings)
+        }
 
     def criterion(self, period):
         date_format = "%d-%m-%Y"
-        date = (datetime.now().date() - timedelta(self.PERIODS[period]))
-        date = date.strftime(date_format)
+        days = timedelta(self.periods.get(period, self.default_period))
+        date = (datetime.now().date() - days).strftime(date_format)
         date = int(datetime.strptime(date, date_format).strftime("%s"))
 
         return {
@@ -27,56 +107,6 @@ class AffectedInstances(IChart):
                     "Gt": date,
                 }
             }
-        }
-
-    @staticmethod
-    def id():
-        return "affected_instances"
-
-    @staticmethod
-    def affected_iscs_id(findings):
-        return list(
-            set(
-                [
-                    finding.resource.details.instance_id for finding
-                    in findings
-                ]
-            )
-        )
-
-    @staticmethod
-    def finding_types(findings):
-        return list(
-            set(
-                [finding.type for finding in findings]
-            )
-        )
-
-    @staticmethod
-    def _sum_of_findings_by_isc(instance, findings):
-        return sum(
-            finding.resource.details.instance_id == instance for finding
-            in findings
-        )
-
-    def _segments(self, instance, findings):
-        def segment(_type):
-            return {
-                "key": self.finding_types(findings).index(_type),
-                "value": sum(
-                    finding.type == _type and
-                    finding.resource.details.instance_id == instance
-                    for finding in findings
-                )
-            }
-
-        return [segment(_type) for _type in self.finding_types(findings)]
-
-    def _data(self, instance, findings):
-        return {
-            "key": self.affected_iscs_id(findings).index(instance),
-            "value": self._sum_of_findings_by_isc(instance, findings),
-            "segments": self._segments(instance, findings)
         }
 
     def build(self, findings):
