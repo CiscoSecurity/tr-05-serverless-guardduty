d89baada4a990616bc6d417bc4b61e2fec4a6841
Taras Maliuchenko <tmali@softserveinc.com>
2021-09-29 13:44:01 +0300

    Fix imports

--- code/api/tiles/__init__.py
+++ code/api/tiles/__init__.py
@@ -1,2 +1,2 @@
-from .affected_instances import AffectedInstances  # noqa: F401
-from .events_per_day import EventsPerDay           # noqa: F401
+from api.tiles.affected_instances import AffectedInstances  # noqa: F401
+from api.tiles.events_per_day import EventsPerDay           # noqa: F401
