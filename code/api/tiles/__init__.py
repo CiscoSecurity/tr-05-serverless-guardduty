1e416759542aa9ebadc2823d7a8ef98fa69fffa2
Taras Maliuchenko <tmali@softserveinc.com>
2021-09-30 14:34:50 +0300

    Implement Top 10 findings tile

--- code/api/tiles/__init__.py
+++ code/api/tiles/__init__.py
@@ -1,2 +1,3 @@
 from api.tiles.affected_instances import AffectedInstances  # noqa: F401
 from api.tiles.events_per_day import EventsPerDay           # noqa: F401
+from api.tiles.top_ten_findings import TopTenFindings       # noqa: F401
