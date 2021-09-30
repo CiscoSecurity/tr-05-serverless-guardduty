1df41b217d249c89a918772774b10df285724c2c
Taras Maliuchenko <tmali@softserveinc.com>
2021-09-30 14:34:50 +0300

    Implement Top 10 findings tile

---
+++ code/api/tiles/top_ten_findings.py
@@ -0,0 +1,74 @@
+from api.tiles.factory import ITile
+
+
+class TopTenFindings(ITile):
+    @property
+    def _id(self):
+        return "top_ten_findings"
+
+    @property
+    def _type(self):
+        return "markdown"
+
+    @property
+    def _tags(self):
+        return [self._id]
+
+    @property
+    def _title(self):
+        return "Top 10 Findings by count"
+
+    @property
+    def _description(self):
+        return (
+            f"{self._title} tile provides a list "
+            f"of the top 10 findings by count."
+        )
+
+    @property
+    def _short_description(self):
+        return self._description
+
+    @property
+    def _periods(self):
+        return {
+            "last_24_hours": 1,
+            "last_7_days": 7,
+            "last_30_days": 30
+        }
+
+    @property
+    def limit(self):
+        return 10
+
+    @property
+    def sort_criteria(self):
+        return {
+            "AttributeName": "service.count",
+            "OrderBy": "DESC"
+        }
+
+    @staticmethod
+    def _data(findings):
+        row = "| {number} | {description} | {count} |"
+        table = [
+            "|  №  | Description | Count |",
+            "| --- | ----------- | ----- |"
+        ]
+        table.extend(
+            [row.format(
+                number=index+1,
+                description=item.Description,
+                count=item.Service.Count
+            ) for index, item in enumerate(findings)]
+        )
+        return {
+            "data": table
+        }
+
+    def tile_data(self, findings, period):
+        data = super(TopTenFindings, self).tile_data(period)
+        data.update(
+            self._data(findings)
+        )
+        return data
