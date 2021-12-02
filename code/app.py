18865fee2ec9ec3fa168bf0aa9297d7a983a8345
oshynk <80475008+oshynk@users.noreply.github.com>
2021-12-02 14:09:18 +0200

    [CCTRI-3398] Add traceback to log file (#43)

--- code/app.py
+++ code/app.py
@@ -1,3 +1,5 @@
+import traceback
+
 from flask import Flask, jsonify
 
 from api.dashboard import dashboard_api
@@ -22,7 +24,7 @@ app.register_blueprint(watchdog_api)
 
 @app.errorhandler(Exception)
 def handle_error(exception):
-    app.logger.error(exception)
+    app.logger.error(traceback.format_exc())
     code = getattr(exception, 'code', 500)
     message = getattr(exception, 'description', 'Something went wrong.')
     reason = '.'.join([
@@ -36,7 +38,7 @@ def handle_error(exception):
 
 @app.errorhandler(TRFormattedError)
 def handle_tr_formatted_error(exception):
-    app.logger.error(exception)
+    app.logger.error(traceback.format_exc())
     return jsonify_errors(exception.json)
 
 
